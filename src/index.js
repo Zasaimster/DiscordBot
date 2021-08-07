2f8b1ce53fc54098de1f7dbe5c6c156b0f7cecf6
Zasaimster <Zasaimster@gmail.com>
2021-08-07 02:00:37 -0700

    removed firestore json file and stored its values in env

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ client.login(process.env.DISCORDJS_BOT_TOKEN);
 
 //initialize firestore connection
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
+	credential: admin.credential.cert({
+		type: 'service_account',
+		project_id: 'zasbot-6865a',
+		private_key_id: process.env.PRIVATE_KEY_ID,
+		private_key: process.env.PRIVATE_KEY,
+		client_email: process.env.CLIENT_EMAIL,
+		client_id: process.env.CLIENT_ID,
+		auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+		token_uri: 'https://oauth2.googleapis.com/token',
+		auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+		client_x509_cert_url: process.env.X509_CERT_URL,
+	}),
 });
 
 //setup db
@@ -21,7 +31,7 @@ client.on('ready', () => {
 	//silver to diamond: https://i.imgur.com/THA3rVI.jpg
 	//monkey: https://i.imgur.com/othLBaA.png
 	//client.user.setAvatar('https://i.imgur.com/THA3rVI.jpg');
-	client.user.setActivity('-help to get started');
+	client.user.setActivity('-help');
 });
 
 const commandHandler = require('./commandHandler');
